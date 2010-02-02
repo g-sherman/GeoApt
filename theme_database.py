cf6598d2e242e9a93294ee92597f4b2a69b0f4d3
Gary Sherman <gsherman@mrcc.com>
2010-02-01 19:51:09 -0900

    added add_folder method to the ThemeDatabase class

--- theme_database.py
+++ theme_database.py
@@ -7,3 +7,8 @@ class ThemeDatabase:
     def create_schema(self, db):
         print "Creating schema in SQLITE3 database\n"
         db.exec_("create table themes (id int primary key, name text, path text)")
+
+    @classmethod
+    def add_folder(self, db, theme, parent_id):
+        print "Adding folder to database\n"
+        db.exec_("insert into themes values(0, '%s', '%s')" % [theme.name, theme.path])
