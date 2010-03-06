01de0f675feb0ddd811a834d86acea5b8b20a6d3
Gary Sherman <gsherman@mrcc.com>
2010-03-06 12:54:08 -0900

    Command file to start GeoApt from within an OSGeo4w shell on Windows

---
+++ run.cmd
@@ -0,0 +1,5 @@
+REM Command file to setup GeoApt from within an OSGeo4w shell
+set PATH=%PATH%;c:\osgeo4w\apps\qgis-unstable\bin
+set PYTHONPATH=%PYTHONPATH%;c:\osgeo4w\apps\qgis-unstable\python
+set QGISHOME=c:\osgeo4w\apps\qgis-unstable        
+python GeoApt.py
